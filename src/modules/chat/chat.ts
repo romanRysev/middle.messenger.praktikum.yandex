d282d7df8897ae7b1217c53c1c51cf7fdbb4b540
Roman Rysev <roman.rysev@sitronics-kt.ru>
2022-08-12 17:49:52 +0300

    доделал страницу с чатами

--- src/modules/chat/chat.ts
+++ src/modules/chat/chat.ts
@@ -1,3 +1,4 @@
+import { Input } from "../../components/input/input";
 import { ShortView } from "../../components/short-view/short-view";
 import { Block, Props } from "../block/block";
 import tpl from "./chat.hbs";
@@ -40,12 +41,28 @@ export class Chats extends Block {
       shortView: chats,
       list,
       messages: currentItem.item.messages,
+      messageInput: new Input({ placeholder: "text...", name: "message", class: "chat__message-input" }),
+      searchInput: new Input({ placeholder: "search" }),
+      callbacks: {
+        submit: (event) => {
+          event.preventDefault();
+          const form = document.forms.newMessage;
+          const { message } = form.elements;
+          currentItem.item.messages.push({ text: message.value, time: new Date().toUTCString(), isMy: true });
+          this.setProps({ messages: currentItem.item.messages });
+          message.focus();
+        },
+      },
       ...props,
     });
   }
 
   render(): string {
     return this.compile(tpl);
+  }
+
+  componentDidMount() {
+    this.getContent()?.addEventListener("submit", this.props?.callbacks?.submit?.bind(this));
   }
 }
 
